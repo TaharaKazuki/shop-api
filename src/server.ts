5f783f18754875edcef3e479dab455ceefaea964
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-19 14:48:20 +0900

    update

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,16 @@ app.use(morgan('tiny'))
 const api = process.env.API_URL
 
 app.get(`${api}/products`, (req, res) => {
-  res.send('hello API')
+  const product = {
+    id: 1,
+  }
+  res.send(product)
+})
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body
+  console.info('通過', newProduct)
+  res.send(newProduct)
 })
 
 app.listen(3000, () => {
